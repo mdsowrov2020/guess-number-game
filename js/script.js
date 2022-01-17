7558b8b200078860e474cb167f7e2f23029a6349
Antu <antukhadem17@gmail.com>
2022-01-17 21:15:40 +0600

    reset functionality added

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 'use strict';
 
-const secretNumber = Math.trunc(Math.random() * 20) + 1;
+let secretNumber = Math.trunc(Math.random() * 20) + 1;
 let score = 20;
 // document.querySelector('.numVal').textContent = secretNumber;
 
@@ -15,14 +15,41 @@ document.querySelector('.guessNum').addEventListener('click', function () {
     document.querySelector('.decision').textContent = 'Correct Number';
     document.querySelector('.numVal').textContent = secretNumber;
     document.querySelector('.number').style.cssText =
-      'background-color: #90EE90; width: 40%; transition: all .6s linear';
+      'background-color: #90EE90; width: 80%; transition: all .6s linear';
+
+    document.querySelector('.bg').style.cssText =
+      'background: url(../images/num.jpg); ';
   } else if (inpVal > secretNumber) {
-    document.querySelector('.decision').textContent = 'too high';
-    score--;
-    document.querySelector('.score').textContent = score;
+    if (score > 1) {
+      document.querySelector('.decision').textContent = 'too high';
+      score--;
+      document.querySelector('.score').textContent = score;
+    } else {
+      document.querySelector('.decision').textContent = 'You lost the game!';
+    }
   } else if (inpVal < secretNumber) {
-    document.querySelector('.decision').textContent = 'too low';
-    score--;
-    document.querySelector('.score').textContent = score;
+    if (score > 1) {
+      document.querySelector('.decision').textContent = 'too low';
+      score--;
+      document.querySelector('.score').textContent = score;
+    } else {
+      document.querySelector('.decision').textContent = 'You lost the game!';
+    }
   }
+});
+
+// reset
+
+document.querySelector('.reset').addEventListener('click', function () {
+  score = 20;
+  secretNumber = Math.trunc(Math.random() * 20) + 1;
+
+  document.querySelector('.decision').textContent = 'Start Guessing..';
+  document.querySelector('.score').textContent = score;
+  document.querySelector('.inpVal').value = '';
+
+  document.querySelector('.number').style.cssText =
+    'background-color: rgba(250, 250, 250, 0.9); width: 25%; transition: all .6s linear';
+  document.querySelector('.numVal').textContent = '?';
+  document.querySelector('.bg').style.background = '#82D0FC';
 });
